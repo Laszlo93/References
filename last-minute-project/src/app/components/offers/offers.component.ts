27e2b84d82d2bebe24f7d38c892ef363e5567036
Dworschák Gábor <solidarity@webmail.hu>
2022-02-15 00:51:38 +0100

    1. day codes

--- last-minute-project/src/app/components/offers/offers.component.ts
+++ last-minute-project/src/app/components/offers/offers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PackageModel } from 'src/app/models/package.model';
+import { PackageService } from 'src/app/services/package.service'
 
 @Component({
   selector: 'app-offers',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffersComponent implements OnInit {
 
-  constructor() { }
+  public travelPackages: PackageModel[] = [];
+  public selectedPackages: PackageModel[] = [];
+  public inputValue: string = "";
+  public canSubmit: boolean = true;
+  public cities: string[] = [];
+
+  constructor(private packageService: PackageService) { }
 
   ngOnInit(): void {
+    this.packageService.getPackages().subscribe({
+      next: (packages: PackageModel[]) => {
+        this.travelPackages = packages;
+        this.selectedPackages = packages;
+        this.cities = this.travelPackages.map(data => data.city).filter((city, index) => {
+          return this.travelPackages.map(data => data.city).indexOf(city) == index;
+        })
+      },
+      error: (e) => console.log(e),
+      complete: () => console.log('Done', this.travelPackages, this.cities)
+    })
+
+  }
+
+  public searchInputChanged(event: any): void{
+    this.inputValue = event.target.value;
+    this.canSubmit = this.inputValue === "";
+  }
+
+  public searchCity(city: string): void {
+    this.selectedPackages = this.travelPackages.filter(data => data.city.includes(city));
   }
 
 }
+
