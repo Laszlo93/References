c9d8bcb5888bde1541820cab72e0fdee9f181142
Laszlo <gaborlaszlo93@gmail.com>
2022-02-20 12:22:53 +0100

    Firestore has been added

--- last-minute-project/src/app/services/package.service.ts
+++ last-minute-project/src/app/services/package.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { filter, Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 import { PackageModel } from '../models/package.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,33 @@ export class PackageService {
   private packages?: Observable<PackageModel[]>;
   private package?: Observable<PackageModel>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private afs: AngularFirestore) { }
+
+  // public getPackages(): Observable<PackageModel[]> {
+  //   return this.http.get<PackageModel[]>(this.PACKAGE_URL);
+  // }
 
   public getPackages(): Observable<PackageModel[]> {
-    return this.http.get<PackageModel[]>(this.PACKAGE_URL);
+    const postsCollection = this.afs.collection<PackageModel>("packages");
+    return postsCollection.get().pipe(
+      map((packages) => packages.docs.map((onePackage) => {
+        const convertedPost: PackageModel = onePackage.data();
+        return convertedPost;
+      }))
+    )
   }
 
+  // public getPackage(packageName: string): Observable<PackageModel[]> {
+  //   return this.http.get<PackageModel[]>(`${this.PACKAGE_URL}/?name=${packageName}`);
+  // }
+
   public getPackage(packageName: string): Observable<PackageModel[]> {
-    return this.http.get<PackageModel[]>(`${this.PACKAGE_URL}/?name=${packageName}`);
+    const postsCollection = this.afs.collection<PackageModel>("packages", ref => ref.where("name", "==", packageName));
+    return postsCollection.get().pipe(
+      map((packages) => packages.docs.map((onePackage) => {
+        const convertedPost: PackageModel = onePackage.data();
+        return convertedPost;
+      }))
+    )
   }
 }
